5cf0d09aefd3a27da34e4e152cdbda51e1af03c6
Robin Swenson-Healey <rjswenson@gmail.com>
2013-08-11 14:56:44 -0700

    adds css styling and js list with text field

--- todos.js
+++ todos.js
@@ -1,1 +1,22 @@
-alert('hello world');
+
+function addList(myTable) {
+
+  var table = document.getElementById(myTable);
+  var row = table.insertRow(0);
+
+  var text = document.getElementById('AddBox').value;
+
+  var cell = row.insertCell(0);
+    var element1 = document.createElement("input");
+      element1.type = "text";
+      element1.style.width ="100%";
+      element1.value = text;
+      cell.appendChild(element1);  
+}
+
+function deleteRow(row)  {
+
+  var row = row.parentNode.parentNode;
+  row.parentNode.removeChild(row);
+
+}

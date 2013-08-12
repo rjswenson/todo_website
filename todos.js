f7bea1ff7ba823c9009fc8d2f68f19b1c18fc20e
Robin Swenson-Healey <rjswenson@gmail.com>
2013-08-11 17:16:28 -0700

    stylized list items to show deleteability on mouseover

--- todos.js
+++ todos.js
@@ -8,14 +8,15 @@ function addList(myTable) {
   var text = document.getElementById('AddBox').value;
   var cell = row.insertCell(0);
   
-  
-
   var element1 = document.createElement("input");
-      element1.type = "text";
-      element1.style.width ="100%";
-      element1.value = text;
-      //magic 'delete' juice below
-      element1.onfocus = function() {table.deleteRow(this.parentNode.parentNode.rowIndex);}
-      cell.appendChild(element1);  
+  element1.type = "text";
+  element1.style.width ="100%";
+  element1.value = text;
+  element1.title = "click to delete"
+  //magic 'delete' juice below
+  element1.onfocus = function() {
+        table.deleteRow(this.parentNode.parentNode.rowIndex);
+  }
+  cell.appendChild(element1);  
 }
 
